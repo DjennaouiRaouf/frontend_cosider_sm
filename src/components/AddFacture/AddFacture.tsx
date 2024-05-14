b568886542a9917f471bbc1f1bd61b77ce915d4f
PC <djennaoui.raouf@gmail.com>
2024-05-14 15:10:16 +0200

    init

--- src/components/AddFacture/AddFacture.tsx
+++ src/components/AddFacture/AddFacture.tsx
@@ -186,7 +186,7 @@ const AddFacture: React.FC<AddFactureProps> = ({refresh}) => {
                                                           }
                                                           {
                                                               field.count &&
-                                                              <span className="badge bg-primary">{field.count}</span>
+                                                              <span className="badge" style={{marginLeft:5,background: "#df162c"}}>{field.count}</span>
                                                           }
                                                       </strong>
                                                   </label>
