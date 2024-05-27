37766bccaca012c8f2724b1cc5170e1bf202d954
PC <djennaoui.raouf@gmail.com>
2024-05-27 09:51:34 +0200

    init

--- src/components/ActionRenderer/OptionInvoice/InvoicePrinter/InvoicePrinter.tsx
+++ src/components/ActionRenderer/OptionInvoice/InvoicePrinter/InvoicePrinter.tsx
@@ -53,102 +53,202 @@ const InvoicePrinter = forwardRef<HTMLDivElement, InvoicePrinterProps>((props, r
 
   return (
       <div ref={ref} className={"print-only"} style={{width: "100%", height: '842px',margin:0}}>
-        <div style={{paddingLeft:'2cm',paddingRight:'2cm'}}>
-          <label className="form-label mt-3 mb-3" style={{fontSize: '12px'}}>
-              Le: {props.data.date}
-          </label>
-          <div className={'w-100'}>
-              <h1 className="text-center" style={{textAlign: "center", fontSize: '15px', textDecoration: "underline"}}>
-                  Facture N°&nbsp;{props.data.numero_facture}
-              </h1>
-          </div>
-
-
-          <label className="form-label mt-3 mb-3 w-100" style={{fontSize: '12px'}}>
-              <strong>Situation de Travaux N°:</strong> {props.data.num_situation}
-          </label>
-          <hr/>
-          <div className={'w-100'}>
-              <label className="form-label" style={{fontSize: '12px'}}>
-                  <strong>Marché:</strong> {props.data.marche} du {props.data.signature} <br/>
-                  <strong>Objet:</strong> {props.data.projet} <br/>d'un montant
-                  de {Humanize(props.data.montant_marche)} DA <br/>
-                  <span style={{textDecoration: "underline", fontSize: '12px'}}><strong>REF:</strong> </span>
-                  &nbsp;
-                  <strong style={{fontSize: '12px'}}>Pole:</strong>{props.data.pole} &nbsp;
-                  <strong style={{fontSize: '12px'}}>NT:</strong>{props.data.num_travail}&nbsp;
-                  <strong style={{fontSize: '12px'}}>Client:</strong>{props.data.client}&nbsp;
-
+          <div style={{paddingLeft: '2cm', paddingRight: '2cm'}}>
+              <label className="form-label mt-3 mb-3" style={{fontSize: '12px'}}>
+                  Le: {props.data.date}
               </label>
-
-          </div>
-          <hr/>
-          <div className={'mt-3 mb-3 w-100'}>
-              <label className="form-label" style={{fontSize: '12px'}}>
-                  Selon la situation des travaux du {props.data.du} au {props.data.au} : <br/><br/>
-                  Montant cumulé des traveaux réalisés au {props.data.au} en
-                  (HT) {Humanize(props.data.montant_cumule)} DA <br/>
-                  Montant cumulé précédemment des traveaux réalisés au {props.data.du} en
-                  (HT) {Humanize(props.data.montant_precedent)} DA <br/>
+              <div className={'w-100'}>
+                  <h1 className="text-center"
+                      style={{textAlign: "center", fontSize: '15px', textDecoration: "underline"}}>
+                      Facture N°&nbsp;{props.data.numero_facture}
+                  </h1>
+              </div>
 
 
-                  Montant cumulé des traveaux réalisés du {props.data.du} au {props.data.au} en
-                  (HT) {Humanize(props.data.montant)} DA <br/>
-
-                  RG : {extra.rg}% <br/> Montant de retenue de garantie en
-                  (HT) {Humanize(props.data.montant_rg)} DA <br/>
-                  Taux de rabais : {props.data.rabais}% <br/> Montant du rabais en (HT)
-                  : {Humanize(props.data.montant_rb)} DA <br/>
-                  TVA : {extra.tva}% <br/>
-                  Montant de la
-                  taxe {Humanize((props.data.tva / 100) * props.data.montant)} DA <br/>
-
-
+              <label className="form-label mt-3 mb-3 w-100" style={{fontSize: '12px'}}>
+                  <strong>Situation de Travaux N°:</strong> {props.data.num_situation}
               </label>
-          </div>
+              <hr/>
+              <div className={'w-100'}>
+                  <label className="form-label" style={{fontSize: '12px'}}>
+                      <strong>Marché:</strong> {props.data.marche} du {extra.signature} <br/>
+                      <strong>Objet:</strong> {extra.projet} <br/>d'un montant
+                      de {Humanize(extra.montant_marche)} DA en HT <br/>
+                      <span style={{textDecoration: "underline", fontSize: '12px'}}><strong>REF:</strong> </span>
+                      &nbsp;
+                      <strong style={{fontSize: '12px'}}>Pole:</strong>{extra.pole} &nbsp;
+                      <strong style={{fontSize: '12px'}}>NT:</strong>{extra.nt}&nbsp;
+                      <strong style={{fontSize: '12px'}}>Client:</strong>{extra.client}&nbsp;
 
-          {
-              props.data.avf !== 0 &&
+                  </label>
+
+              </div>
+              <hr/>
               <div className={'mt-3 mb-3 w-100'}>
-                  <label className="form-label " style={{fontSize: '12px', width: '100%'}}>
-                      <strong>Avance Forfaitaire </strong>:{Humanize(props.data.avf)} DA
+                  <label className="form-label" style={{fontSize: '12px'}}>
+                      Selon la situation des travaux du {props.data.du} au {props.data.au} : <br/><br/>
+
+                      { props.data.montant_precedent !== 0 &&
+                          <>
+
+                              <div className="input-group w-100">
+                                  <label className="form-label  w-90" style={{fontSize: '12px'}}>Montant cumulé des
+                                      traveaux
+                                      réalisés au {props.data.du} en
+                                      (HT)
+                                      :</label>
+                                  <label className="form-label  w-10"
+                                         style={{fontSize: '12px'}}><strong>{Humanize(props.data.montant_precedent)} DA</strong>
+                                  </label>
+                              </div>
+                          </>
+                      }
+
+                      <div className="input-group w-100">
+                          <label className="form-label  w-90" style={{fontSize: '12px'}}>Montant cumulé des traveaux
+                              réalisés au {props.data.au} en
+                              (HT)
+                              :</label>
+                          <label className="form-label  w-10"
+                                 style={{fontSize: '12px'}}><strong>{Humanize(props.data.montant_cumule)} DA</strong>
+                          </label>
+                      </div>
+
+                      <div className="input-group w-100">
+                          <label className="form-label  w-90" style={{fontSize: '12px'}}>Montant cumulé des traveaux réalisés du {props.data.du} au {props.data.au} en
+                      (HT)
+                              :</label>
+                          <label className="form-label  w-10"
+                                 style={{fontSize: '12px'}}><strong>{Humanize(props.data.montant)} DA</strong>
+                          </label>
+                      </div>
+                      {extra.rg !== 0 &&
+                          <>
+
+                              <div className="input-group w-100">
+                                  <label className="form-label  w-50" style={{fontSize: '12px'}}>Montant de la Retenue
+                                      de Garantie
+                                      ({extra.rg}%)
+                                      :</label>
+                                  <label className="form-label  w-50"
+                                         style={{fontSize: '12px'}}><strong>{Humanize(props.data.montant_rg)} DA </strong>
+                                  </label>
+                              </div>
+                          </>
+
+                      }
+                      {extra.rb !== 0 &&
+                          <>
+                              <div className="input-group w-100">
+                                  <label className="form-label  w-50" style={{fontSize: '12px'}}>Montant du Rabais
+                                      ({extra.rb}%)
+                                      :</label>
+                                  <label className="form-label  w-50"
+                                         style={{fontSize: '12px'}}><strong>{Humanize(props.data.montant_rb)} DA </strong>
+                                  </label>
+                              </div>
+
+
+                          </>
+                      }
+                      {
+                          extra.tva !== 0 &&
+                          <>
+                              <div className="input-group w-100">
+                                  <label className="form-label  w-50" style={{fontSize: '12px'}}>Montant de la TVA
+                                      ({extra.tva}%)
+                                      :</label>
+                                  <label className="form-label  w-50"
+                                         style={{fontSize: '12px'}}><strong>{Humanize((extra.tva / 100) * props.data.montant)} DA</strong>
+                                  </label>
+                              </div>
+
+                          </>
+                      }
+
+
                   </label>
               </div>
 
-          }
-          {props.data.ava !== 0 &&
+              {
+                  props.data.avf !== 0 &&
+                  <div className={'mt-3 mb-3 w-100'}>
+                      <div className="input-group w-100">
+                          <label className="form-label  w-50" style={{fontSize: '12px'}}>Avance Forfaitaire
+                              :</label>
+                          <label className="form-label  w-50"
+                                 style={{fontSize: '12px'}}><strong>{Humanize(props.data.avf)} DA</strong> </label>
+                      </div>
+                  </div>
+
+              }
+              {
+                  props.data.ave !== 0 &&
+                  <div className={'mt-3 mb-3 w-100'}>
+                      <div className="input-group w-100">
+                          <label className="form-label  w-50" style={{fontSize: '12px'}}>Avance Exceptionnelle
+                              :</label>
+                          <label className="form-label  w-50"
+                                 style={{fontSize: '12px'}}><strong>{Humanize(props.data.ave)} DA</strong> </label>
+                      </div>
+
+
+                  </div>
+
+              }
+
+              {props.data.ava !== 0 &&
+                  <div className={'mt-3 mb-3 w-100'}>
+                      <div className="input-group w-100">
+                          <label className="form-label  w-50" style={{fontSize: '12px'}}>Avance sur Appros
+                              :</label>
+                          <label className="form-label  w-50"
+                                 style={{fontSize: '12px'}}><strong>{Humanize(props.data.ava)} DA</strong> </label>
+                      </div>
+
+                  </div>
+              }
+
+              {extra.tva === 0 &&
+
+                  <div className={'mt-3 mb-3 w-100'}>
+                      <label className="form-label text-center" style={{fontSize: '12px', width: '100%'}}>
+                          <strong>EXONEREE DES TAXES</strong>
+                      </label>
+                  </div>
+              }
+
+
               <div className={'mt-3 mb-3 w-100'}>
-                  <label className="form-label " style={{fontSize: '12px', width: '100%'}}>
-                      <strong>Avance sur Appros</strong>:{Humanize(props.data.ava)} DA
-                  </label>
+              <div className="input-group w-100">
+                      <label className="form-label  w-50" style={{fontSize: '12px'}}>MONTANT DE LA FACTURE EN (HT)
+                          :</label>
+                      <label className="form-label  w-50"
+                             style={{fontSize: '12px'}}>{Humanize(props.data.montant_factureHT)} DA </label>
+                  </div>
               </div>
-          }
+              <div className={'mt-3 mb-3 w-100'}>
+                  <div className="input-group w-100">
+                      <label className="form-label  w-50" style={{fontSize: '12px'}}>MONTANT DE LA FACTURE EN
+                          (TTC)
+                          :</label>
+                      <label className="form-label  w-50"
+                             style={{fontSize: '12px'}}><strong>{Humanize(props.data.montant_factureTTC)} DA </strong></label>
+                  </div>
+              </div>
 
-          {props.data.tva === "0.00" &&
 
               <div className={'mt-3 mb-3 w-100'}>
-                  <label className="form-label text-center" style={{fontSize: '12px', width: '100%'}}>
-                      <strong>EXONEREE DES TAXES</strong>
-                  </label>
+                  <div className="input-group w-100">
+                      <label className="form-label  w-50" style={{fontSize: '12px'}}>Arretée la présente facture à la
+                          somme de
+                          :</label>
+                      <label className="form-label  w-50"
+                             style={{fontSize: '12px'}}><strong>{props.data.somme}</strong> </label>
+                  </div>
               </div>
-          }
 
-
-          <div className={'mt-3 mb-3 w-100'}>
-              <label className="form-label" style={{fontSize: '12px',width:'100%'}}>
-                  MONTANT DE LA FACTURE EN (HT) {Humanize(props.data.montant_factureHT)} DA <br/>
-                  MONTANT NET À PAYER À L'ENTREPRISE EN (TTC) {Humanize(props.data.montant_factureTTC)} DA <br/>
-
-              </label>
           </div>
-
-          <div className={'mt-3 mb-3 w-100'}>
-              <label className="form-label" style={{fontSize: '12px' ,width:"100"}}>
-                  Arretée la présente facture à la somme de <strong>{props.data.somme}</strong>
-              </label>
-          </div>
-
-        </div>
       </div>
   )
 });
