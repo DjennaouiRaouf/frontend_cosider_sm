ee917268c7e30b33fbd6f97e7e2371ce835c7a36
PC <djennaoui.raouf@gmail.com>
2024-05-29 10:56:30 +0200

    init

--- src/components/ScrollToTopBtn/ScrollToTopBtn.tsx
+++ src/components/ScrollToTopBtn/ScrollToTopBtn.tsx
@@ -13,17 +13,17 @@ const ScrollToTopBtn: React.FC<any> = () => {
 
      return (
          <>
-             <div className="my-3"><a className="btn btn-primary btn-lg me-2 pulse-wave" role="button"
+
+             <a className="btn  btn-sm me-2 wave-pulse" role="button"
                                       onClick={handleScrollToTop}
                                       style={{
                                           "position": "fixed",
                                           "bottom": "20px",
                                           "right": "30px",
-                                          "backgroundColor":"#df162c",
                                           "zIndex": "99"
-                                      }}><i className="fas fa-caret-up" style={{fontSize:16}}></i>
+                                      }}><i className="fas fa-caret-up" style={{color:"white",fontSize:20}}></i>
 
-                                              </a></div>
+                                              </a>
 
          </>
      );
